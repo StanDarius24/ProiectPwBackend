2ad6f2df3c47de5b2c852affe786bf9b529e30b9
StanDarius24 <standarius24@gmail.com>
2020-10-08 22:50:17 +0300

    Legam baza de date mysql, testam cu postman, invatam chestii noi :D

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+const cors = require('cors');
+/*
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            title: "Shop API",
+            description: "Backend Api",
+            contact: {
+                name: 'Amazing Developer'
+            },
+            servers: "http://localhost:3636"
+        }
+    },
+    apis: ["app.js", ".routes/*.js"]
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+*/
+/* CORS */
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'DELETE', 'PATCH', 'POST'],
+    allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept'
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Import Routes
+
+const productsRouter = require('./routes/products');
+
+
+
+app.use('/api/products', productsRouter);
+
+
+module.exports = app;
---
+++ routes/products.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const {database} = require('../config/helpers');
+
+/* GET ALL PRODUCTS */
+router.get('/', function (req, res) {       // Sending Page Query Parameter is mandatory http://localhost:3636/api/products?page=1
+    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1;
+    const limit = (req.query.limit !== undefined && req.query.limit !== 0) ? req.query.limit : 10;   // set limit of items per page
+    let startValue;
+    let endValue;
+    if (page > 0) {
+        startValue = (page * limit) - limit;     // 0, 10, 20, 30
+        endValue = page * limit;                  // 10, 20, 30, 40
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+    database.table('produse as p')
+
+        .withFields([
+            'p.name as name',
+            'p.pret as pret',
+            'p.descriere as descriere',
+            'p.datalansarii as data',
+            'p.picture as img',
+            'p.id as id'
+        ])
+        .slice(startValue, endValue)
+        .sort({id: .1})
+        .getAll()
+        .then(prods => {
+            if (prods.length > 0) {
+                res.status(200).json({
+                    count: prods.length,
+                    products: prods
+                });
+            } else {
+                res.json({message: "No products found"});
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+module.exports = router;
